73c2d393ad92f80431e60bd4fcaf1b6794d6fcfb
EdsonMorroNogueira <edson.nogueira@sptech.school>
2023-05-29 14:43:41 -0300

    Usuário já está dinâmico, routes do Node já estão funcionando e falta pintar pela validação

---
+++ Site/src/models/medidaModel.js
@@ -0,0 +1,68 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(idRegistro, limite_linhas) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas}
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        momento,
+                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
+                    from medida
+                    where fk_aquario = ${idRegistro}
+                    order by id desc`;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+         temperatura, 
+        umidade,
+                        dtHora,
+                        DATE_FORMAT(dtHora,'%H:%i:%s') as momento_grafico
+                    from registro
+                    where fkSensor = ${idRegistro}
+                    order by idRegistro desc limit ${limite_linhas}`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(idRegistro) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 1
+        temperatura, 
+        umidade,  
+                        CONVERT(varchar, dtHora, 108) as momento_grafico, 
+                        fkSensor
+                        from registro where fkSensor = ${idRegistro} 
+                    order by id desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+         temperatura, 
+         umidade,
+                        DATE_FORMAT(dtHora,'%H:%i:%s') as momento_grafico, 
+                        fkSensor 
+                        from registro where fkSensor= ${idRegistro} 
+                    order by idRegistro desc limit 1`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
